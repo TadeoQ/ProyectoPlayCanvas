b5e652573413f0554fa4d667b808390c50b18886
TadeoQ <tadeo_mq28@hotmail.com.com>
2024-09-23 12:50:46 -0600

    proyecto playcanvas

---
+++ __settings__.js
@@ -0,0 +1,22 @@
+window.ASSET_PREFIX = "";
+window.SCRIPT_PREFIX = "";
+window.SCENE_PATH = "2076059.json";
+window.CONTEXT_OPTIONS = {
+    'antialias': true,
+    'alpha': false,
+    'preserveDrawingBuffer': false,
+    'deviceTypes': [`webgl2`, `webgl1`],
+    'powerPreference': "high-performance"
+};
+window.SCRIPTS = [ 196810995, 196810996, 196811019, 196810999, 196811000, 196811001, 196811002, 196811003, 196811005, 196811006, 196811007, 196811008, 196811010, 196811011, 196816391, 196818247, 196818249, 196829763, 196829764, 196911206, 196911594, 196911695 ];
+window.CONFIG_FILENAME = "config.json";
+window.INPUT_SETTINGS = {
+    useKeyboard: true,
+    useMouse: true,
+    useGamepads: false,
+    useTouch: true
+};
+pc.script.legacy = false;
+window.PRELOAD_MODULES = [
+    {'moduleName' : 'Ammo', 'glueUrl' : 'files/assets/196811016/1/ammo.wasm.js', 'wasmUrl' : 'files/assets/196811017/1/ammo.wasm.wasm', 'fallbackUrl' : 'files/assets/196811015/1/ammo.js', 'preload' : true},
+];
